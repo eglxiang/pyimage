8b44b398f7b20132fd0b1bfdc9d230cc92a6ce5e
Xiang Xiang <cnwhu@yahoo.com>
2017-02-02 23:58:18 -0500

    test landmark detection at live

---
+++ test_landmark.py
@@ -0,0 +1,23 @@
+import cv2
+import dlib
+
+# set up some required objects
+video_capture = cv2.VideoCapture()
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+while True:
+	ret, frame = video_capture.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+	clahe_image = clahe.apply(gray)
+
+	detections = detector(clahe_image, 1)
+	for k,d in enumerte(detections):
+		shape = predictor(clahe_image, d)
+		for i in range(1,68):
+			cv2.circle(frame, (shape.part(i).x, shape.part(i).y), 1, (0,0,255), thickness=2)
+
+	cv2.imshow("image", frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
